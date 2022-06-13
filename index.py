655a250f5d0b1a3e76f7bccf7e4d98788c9e7405
jackChen <1272016935@qq.com>
2022-06-13 16:25:40 +0800

    整合bot

--- index.py
+++ index.py
@@ -3,22 +3,21 @@ from urllib.parse import urlparse
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import re,struct, sys, os, time,requests
+import logging
+logging.basicConfig(filename = "logfile.log",
+                    filemode = "w",
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 app = Flask(__name__)
 
-# def start(update: Update, context: CallbackContext) -> None:
 
-#     update.message.reply_text(f'Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with python-telegram-bot!n\ncode{update.get_updates()}')
-# REQUEST_KWARGS={
-#     'proxy_url': 'http://127.0.0.1:10809',
-# }
-updater = Updater(os.environ.get("TOKEN"), use_context=True)
-# updater = Updater('5548984433:AAFl5rTKpJRij88tu6sHqhvYpvogqFCYp88', use_context=True, request_kwargs=REQUEST_KWARGS)
-
-# updater.dispatcher.add_handler(CommandHandler('start', start))
-
-# updater.start_polling()
-# updater.idle()
-
+if os.environ.get("TOKEN"):
+    updater = Updater(os.environ.get("TOKEN"), use_context=True)
+else:
+    REQUEST_KWARGS={
+        'proxy_url': 'http://127.0.0.1:10809',
+    }
+    updater = Updater('5548984433:AAFl5rTKpJRij88tu6sHqhvYpvogqFCYp88', use_context=True, request_kwargs=REQUEST_KWARGS)
 # 首页
 @app.route('/', methods=['GET'])
 def show():
@@ -26,13 +25,16 @@ def show():
 
 # 推送
 @app.route('/u/<path:users>/t/<path:texts>', methods=['POST'])
-def api_query_ip_input(texts,users):
+def message_push(texts,users):
     if not texts and not users:
         return render_template('404.html'), 404
     print(texts,users)
-    updater.send_message(text=texts, chat_id=users)
+    updater.bot.send_message(text=texts, chat_id=users)
     return {'code':200}
-
+# ip
+@app.route('/ip', methods=['GET'])
+def ip():
+    return Response(request.remote_addr, mimetype="text/text")
 # 404
 @app.errorhandler(404)
 def page_not_found(error):
